7fdd4a659b473a365229a2b36ba069a574e6b0da
yangyuao <yang_yuao@outlook.com>
2021-03-26 06:02:43 +0800

    😄

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,18 +1,21 @@
 from django.template import Library
+from django.db.models import Count
+from ..models import Category, Tag, Article, FriendLink
 
 register = Library()
 
 
 @register.simple_tag
 def get_tag_list():
-    pass
+    return Tag.objects.annotate(article_num=Count('article')).filter(article_num__gt=0)
 
 
 @register.simple_tag
 def get_category_list():
-    pass
+    return Category.objects.annotate(article_num=Count('article')).filter(article_num__gt=0)
 
 
 @register.simple_tag
 def get_friend_link_list():
-    pass
+    return FriendLink.objects.filter(is_show=True, is_active=True)
+
